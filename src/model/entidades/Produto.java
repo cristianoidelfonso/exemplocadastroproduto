c7c292aef26150a1126d4b0e12955d66575f2b81
Cristiano Idelfonso da Silva <cristiano_idelfonso@hotmail.com>
2018-10-01 22:17:51 -0300

    Aula segunda feira 01/10/2018

---
+++ src/model/entidades/Produto.java
@@ -0,0 +1,65 @@
+package model.entidades;
+
+import java.time.LocalDate;
+
+public class Produto {
+    
+    private int idproduto;
+    private String nome;
+    private double preco;
+    private String codigo;
+    private int quantidade;
+    private LocalDate validade;
+
+    public Produto() {
+    }
+
+    public int getIdproduto() {
+        return idproduto;
+    }
+
+    public void setIdproduto(int idproduto) {
+        this.idproduto = idproduto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public LocalDate getValidade() {
+        return validade;
+    }
+
+    public void setValidade(LocalDate validade) {
+        this.validade = validade;
+    }
+    
+}
