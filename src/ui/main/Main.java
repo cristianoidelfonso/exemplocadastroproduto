f25a6df98c1ce2c4f570982caa16ef1a663e1987
Cristiano Idelfonso da Silva <cristiano_idelfonso@hotmail.com>
2018-10-02 21:41:38 -0300

    Aula segunda feira 02/10/2018

--- src/ui/main/Main.java
+++ src/ui/main/Main.java
@@ -1,6 +1,31 @@
 package ui.main;
 
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
-public class Main {
+
+public class Main extends Application{
     
+    @Override
+    public void start (Stage stage) throws IOException {
+        
+        Parent root = FXMLLoader.load(getClass().getResource("Principal.fxml"));
+        
+        stage.setTitle("");
+        stage.getIcons().add(new Image("/icones/virtual-machine.96"));
+        
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        
+    }
+    
+    public static void main(String[] args){
+        launch(args);
+    }
 }
