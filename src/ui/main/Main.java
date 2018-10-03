e1ad535c17f7fa277854564ddddc98a68f145d55
Cristiano Idelfonso da Silva <cristiano_idelfonso@hotmail.com>
2018-10-02 21:26:07 -0300

    Aula segunda feira 01/10/2018

--- src/ui/main/Main.java
+++ src/ui/main/Main.java
@@ -1,28 +1,6 @@
 package ui.main;
 
-import java.io.IOException;
-import static javafx.application.Application.launch;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
 public class Main {
     
-    public void start(Stage stage) throws IOException {
-        //Leitura do FXML
-        Parent root = FXMLLoader.load(getClass().getResource("Principal.fxml"));
-
-        //Criação da cena
-        Scene scene = new Scene(root);
-
-        //Configuração da janela
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    //Sempre fixo chamando a execucao do javafx
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
--- src/ui/main/PrincipalController.java
+++ src/ui/main/PrincipalController.java
@@ -1,11 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package ui.main;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
+/**
+ * FXML Controller class
+ *
+ * @author Idelfonso
+ */
 public class PrincipalController implements Initializable {
 
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
