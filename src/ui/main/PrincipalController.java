8edd4e984d52f1c3430133d3de112b30bfaa1be1
Cristiano Idelfonso da Silva <cristiano_idelfonso@hotmail.com>
2018-10-03 22:22:58 -0300

    Aula 03/10/2018 Definindo e dividindo o código em blocos lógicos

--- src/ui/main/PrincipalController.java
+++ src/ui/main/PrincipalController.java
@@ -8,32 +8,40 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import ui.produto.cadastro.*;
 
 /**
  * FXML Controller class
+ *
  * @author Idelfonso
  */
 public class PrincipalController implements Initializable {
 
     @FXML
     private BorderPane principal;
+    @FXML
+    private MenuItem menuItem;
 
     /**
      * Initializes the controller class.
+     *
      * @param url
      * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 
     @FXML
     private void abrirJanelaCadastro(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Cadastro.fxml"));
-        
-        principal.getChildren().setAll(root);
+
     }
-    
+
 }
