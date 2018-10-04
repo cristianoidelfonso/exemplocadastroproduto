062a5824347edcd0da8b42ac5099728ca10e9f86
Cristiano Idelfonso da Silva <cristiano_idelfonso@hotmail.com>
2018-10-03 22:24:11 -0300

    Aula 03/10/2018

--- src/ui/main/PrincipalController.java
+++ src/ui/main/PrincipalController.java
@@ -14,7 +14,6 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import ui.produto.cadastro.*;
 
 /**
  * FXML Controller class
