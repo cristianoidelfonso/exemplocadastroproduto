88f5b7f9475112bceb007c1015f449fd28bd5cce
Cristiano Idelfonso da Silva <cristiano_idelfonso@hotmail.com>
2018-10-04 11:42:42 -0300

    Criação das Telas Principal e CadastroProduto

--- src/ui/produto/cadastro/FXMLCadastroProdutoController.java
+++ src/ui/produto/cadastro/FXMLCadastroProdutoController.java
@@ -1,10 +1,43 @@
 package ui.produto.cadastro;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 public class FXMLCadastroProdutoController implements Initializable {
+
+    @FXML
+    private Label lblTextoRodape1;
+    @FXML
+    private JFXButton btnSalvar;
+    @FXML
+    private JFXButton btnEditar;
+    @FXML
+    private JFXButton btnExcluir;
+    @FXML
+    private JFXTextField txtId;
+    @FXML
+    private JFXTextField txtCodigo;
+    @FXML
+    private JFXTextField txtNome;
+    @FXML
+    private JFXTextField txtPreco;
+    @FXML
+    private JFXTextField txtQuantidade;
+    @FXML
+    private JFXDatePicker dpValidade;
+    @FXML
+    private JFXButton btnFiltrar;
+    @FXML
+    private JFXTextField txtFiltro;
+    @FXML
+    private JFXComboBox<?> cbFiltro;
 
     /**
      * Initializes the controller class.
