d9490dfac392aa3c8ab4d1dc7f6179f7dec9b190
Cristiano Idelfonso da Silva <cristiano_idelfonso@hotmail.com>
2018-10-03 21:41:41 -0300

    Aula 03/10/2018 Definindo e dividindo o código em blocos lógicos

--- src/ui/main/PrincipalController.java
+++ src/ui/main/PrincipalController.java
@@ -1,52 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package ui.main;
 
-import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
-
 
 /**
  * FXML Controller class
+ *
  * @author Idelfonso
  */
 public class PrincipalController implements Initializable {
 
-    @FXML
-    private Label lblRodape;
-    @FXML
-    private Label lblValidade;
-    @FXML
-    private JFXTextField txtId;
-    @FXML
-    private JFXTextField txtCodigo;
-    @FXML
-    private JFXTextField txtNome;
-    @FXML
-    private JFXTextField txtPreço;
-    @FXML
-    private JFXTextField txtQuantidade;
-    @FXML
-    private DatePicker dpDataValidade;
-    @FXML
-    private JFXButton btnSalvar;
-    @FXML
-    private JFXButton btnEditar;
-    @FXML
-    private JFXButton btnExcluir;
-    @FXML
-    private JFXButton btnFiltrar;
-      
-
     /**
      * Initializes the controller class.
-     * @param url
-     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -54,43 +27,7 @@ public class PrincipalController implements Initializable {
     }    
 
     @FXML
-    private void btnSalvarExited(MouseEvent event) {
-        btnSalvar.setStyle("-fx-background-color: #66CDAA; -fx-text-fill: #FFFFFF;");
+    private void abrirJanelaCadastro(ActionEvent event) {
     }
-
-    @FXML
-    private void btnSalvarEntered(MouseEvent event) {
-        btnSalvar.setStyle("-fx-background-color: #008B8B; -fx-text-fill: #FFFFFF;");
-    }
-
-    @FXML
-    private void btnEditarExited(MouseEvent event) {
-        btnEditar.setStyle("-fx-background-color: #66CDAA; -fx-text-fill: #FFFFFF;");
-    }
-
-    @FXML
-    private void btnEditarEntered(MouseEvent event) {
-        btnEditar.setStyle("-fx-background-color: #008B8B; -fx-text-fill: #FFFFFF;");
-    }
-
-    @FXML
-    private void btnExcluirExited(MouseEvent event) {
-        btnExcluir.setStyle("-fx-background-color: #66CDAA; -fx-text-fill: #FFFFFF;");
-    }
-
-    @FXML
-    private void btnExcluirEntered(MouseEvent event) {
-        btnExcluir.setStyle("-fx-background-color: #008B8B; -fx-text-fill: #FFFFFF;");
-    }
-
-    @FXML
-    private void btnFiltrarExited(MouseEvent event) {
-        btnFiltrar.setStyle("-fx-background-color: #66CDAA; -fx-text-fill: #FFFFFF;");
-    }
-
-    @FXML
-    private void btnFiltrarEntered(MouseEvent event) {
-        btnFiltrar.setStyle("-fx-background-color: #008B8B; -fx-text-fill: #FFFFFF;");
-    }
-   
+    
 }
