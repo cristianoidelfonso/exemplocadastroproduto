fd1ef0908d6841d88b2a054ec148857cba23beab
Cristiano Idelfonso da Silva <cristiano_idelfonso@hotmail.com>
2018-10-03 21:16:14 -0300

    Aula 03/10/2018 Definindo e dividindo o código em blocos lógicos

--- src/ui/main/PrincipalController.java
+++ src/ui/main/PrincipalController.java
@@ -1,11 +1,14 @@
 package ui.main;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 
 /**
@@ -13,7 +16,10 @@ import javafx.scene.input.MouseEvent;
  * @author Idelfonso
  */
 public class PrincipalController implements Initializable {
+    
 
+// <editor-fold defaultstate="collapsed" desc=" Variáveis ">
+    
     @FXML
     private JFXButton btnSalvar;
     @FXML
@@ -22,6 +28,22 @@ public class PrincipalController implements Initializable {
     private JFXButton btnExcluir;
     @FXML
     private JFXButton btnFiltrar;
+    @FXML
+    private JFXTextField txtId;
+    @FXML
+    private JFXTextField txtCodigo;
+    @FXML
+    private JFXTextField txtNome;
+    @FXML
+    private JFXTextField txtPreço;
+    @FXML
+    private JFXTextField txtQuantidade;
+    @FXML
+    private Label lblData;
+    @FXML
+    private DatePicker dpValidade;
+ 
+// </editor-fold >    
 
     /**
      * Initializes the controller class.
@@ -38,6 +60,7 @@ public class PrincipalController implements Initializable {
         // Abrir a janela de cadastro de produto
     }
 
+// <editor-fold defaultstate="collapsed" desc=" Eventos dos Botões ">
     @FXML
     private void btnSalvarExited(MouseEvent event) {
         // Quando o ponteiro do mouse sair da area do botão.
@@ -85,5 +108,6 @@ public class PrincipalController implements Initializable {
         // Quando o ponteiro do mouse entrar na area do botão.
         btnFiltrar.setStyle("-fx-background-color: #008B8B; -fx-text-fill: #FFFFFF;");
     }
+// </editor-fold>  
     
 }
