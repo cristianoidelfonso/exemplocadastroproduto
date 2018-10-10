091b15fb96c81deabe045a3320a347193d5784e8
Cristiano Idelfonso da Silva <cristiano_idelfonso@hotmail.com>
2018-10-09 21:26:43 -0300

    Aula 09/10/2018 Funcionando

--- src/ui/produto/cadastro/FXMLCadastroProdutoController.java
+++ src/ui/produto/cadastro/FXMLCadastroProdutoController.java
@@ -54,6 +54,8 @@ public class FXMLCadastroProdutoController implements Initializable {
 
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
